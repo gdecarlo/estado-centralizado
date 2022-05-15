c9277f7d76dbf4b1d30e07b9c3c3b182cf354a85
Gaspar De Carlo <gaspar.decarlo@gmail.com>
2022-05-15 18:05:06 -0300

    se renombra el store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 
-export const useStore = defineStore("main", {
+export const useStore = defineStore("nt2", {
   state: () => ({
     _count: 0,
     _config: {},
